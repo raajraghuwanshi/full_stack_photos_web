a807df54f4c43ed4719d3081e38dcdd9f18fbe48
raj raghuwanshi <rajraghuawnshi9479@gmail.com>
2026-04-07 15:29:22 +0530

    add vercel

--- src/lib/AxiosInstance.js
+++ src/lib/AxiosInstance.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://full-stack-backend-4ag5.onrender.com/api", // change if needed
+  baseURL: "https://full-stack-backend-iota-eight.vercel.app/api", // change if needed
   withCredentials: true,
 });
 
